8519bb82687cc0ff249ac6f11a0592f0079e4aa7
黒田学人 <a7108100@gmail.com>
2019-06-05 11:48:40 +0900

    chore: use eslint fix for ts and tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@
  */
 
 import React from 'react'
-import {Platform, StyleSheet, Text, View} from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,7 +16,9 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 })
 
-interface Props {}
+interface Props {
+  test: string
+}
 export default class App extends React.Component<Props> {
   public render() {
     return (
